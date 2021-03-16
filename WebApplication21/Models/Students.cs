b0be02ea9bb8bf3095f2f7e1db374578c019a41c
sreekanthking987@gmail.com <sreekanthking987@gmail.com>
2021-03-16 15:37:42 +0530

    Final commit

---
+++ WebApplication21/Models/Students.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebApplication21.Models
+{
+    public partial class Students
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Course { get; set; }
+        public string Email { get; set; }
+        public string Contact { get; set; }
+    }
+}
---
+++ WebApplication21/Models/StudentsDbContext.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WebApplication21.Models
+{
+    public partial class StudentsDbContext : DbContext
+    {
+        public StudentsDbContext()
+        {
+        }
+
+        public StudentsDbContext(DbContextOptions<StudentsDbContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Students> Students { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("server=LAPTOP-NMP648PR;database=StudentsDb;trusted_connection=true;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Students>(entity =>
+            {
+                entity.ToTable("students");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Contact).HasMaxLength(10);
+
+                entity.Property(e => e.Course)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.Email)
+                    .IsRequired()
+                    .HasMaxLength(40);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(40);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
